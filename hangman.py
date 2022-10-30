57dbff38e536a95af7f465c7ee3db6669fcee92b
Allan Anilson <allananilson@gmail.com>
2022-10-30 07:55:29 +0530

    hangman was modified

--- hangman.py
+++ hangman.py
@@ -13,11 +13,11 @@ def hangman():
     word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
-    lives = 10
+    chances = 10
 
     #getting user input
-    while len(word_letters)>0 and lives>0:
-        print("You have",lives,"chances left and you have used these letters: ", ' '.join(used_letters))
+    while len(word_letters)>0 and chances>0:
+        print("You have",chances,"chances left and you have used these letters: ", ' '.join(used_letters))
         word_list = [letter if letter in used_letters else '-' for letter in word]
         print('Current Word: ', ' '.join(word_list))
 
@@ -27,14 +27,14 @@ def hangman():
             if(user_letter in word_letters):
                 word_letters.remove(user_letter)
             else:
-                lives = lives - 1;
+                chances = chances - 1;
                 print("The letter in not in the word!")
         elif user_letter in used_letters:
             print("You have already guessed this letter!")
         else:
             print("You have entered an invalid character. Please Try Again!")
     
-    if lives == 0:
+    if chances == 0:
         print("Sorry, you ran out of chances! The word was", word)
     else:
         print("Congratulations! You have guessed the word!", word)
